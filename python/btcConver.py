d339f5fc0ec34572843fbb711ffef7654a7b9fd0
blakemiles <blakemiles84@gmail.com>
2016-05-30 17:35:52 -0400

    updated index and stylesheet for resume website

---
+++ python/btcConver.py
@@ -0,0 +1,7 @@
+print "What is the current conversion rate of BTC/USD?" 
+btcRate = raw_input("$ ")
+print "How much would you like to convert? "
+dollar = raw_input("$ ")
+converted = btcRate / dollar
+print converted
+  
